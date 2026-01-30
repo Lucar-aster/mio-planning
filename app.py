9eacfd1d86e78a044501ca65ed2a5f25aee80a00
Lucar-aster <luca.r@astercucine.it>
2026-01-30 10:25:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def render_gantt_fragment(df_plot, lista_op, oggi, x_range, x_dtick, formato_it,
         margin=dict(l=10, r=20, t=10, b=10),
         shapes=shapes,
         automargin=True,
-        xaxis=dict(type="date", side="top", tickmode="array",       # Forza l'uso dei nostri valori
+        xaxis=dict(type="date", side="top", tickmode='linear',       # Forza l'uso dei nostri valori
         tickvals=tick_vals,    # Le posizioni dei giorni
         ticktext=tick_text,    # Le etichette tradotte in italiano
         tickfont=dict(size=10, color="#1E3A8A"),range=x_range, dtick=x_dtick, tickformat="%b<br>%d %a<br>Sett. %V", showgrid=True, gridcolor="#e0e0e0"),
