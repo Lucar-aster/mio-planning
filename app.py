76a485e118f241f140f5b4f1ea4bb60ebf1f7070
Lucar-aster <luca.r@astercucine.it>
2026-02-16 08:08:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown(f"""
         gap: 0.1rem !important;
     }}
     
-    .compact-title {{ display: flex; align-items: center; gap: 5px; padding-top: 2px; margin-bottom: -10px;}}
+    .compact-title {{ display: flex; align-items: center; gap: 5px; padding-top: 2px; margin-bottom: -5px;}}
     .compact-title h1 {{ font-size: 22px !important; color: #1E3A8A; margin: 0; }}
 
     [data-testid="stVerticalBlock"] {{
