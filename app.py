2383aacc0c264232841e27f1099870ed89fa3a12
Lucar-aster <luca.r@astercucine.it>
2026-03-18 15:04:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -706,12 +706,12 @@ def render_gantt_fragment(df_plot, color_map, oggi_dt, x_range, delta_giorni, sh
 
     if selected and "selection" in selected and selected["selection"]["points"]:
     dati_punto = selected["selection"]["points"][0].get("customdata", [])
-    if dati_punto and dati_punto[0] == "LOG_FITTIZIO":
-        task_id = dati_punto[1]
-        data_clic = dati_punto[2] # Ecco la tua data YYYY-MM-DD!
-        modal_gestione_clic(task_id, data_clic)
-    else:
-        modal_edit_log(d[0], d[1], d[2], d[3], d[7], d[6])
+        if dati_punto and dati_punto[0] == "LOG_FITTIZIO":
+            task_id = dati_punto[1]
+            data_clic = dati_punto[2] # Ecco la tua data YYYY-MM-DD!
+            modal_gestione_clic(task_id, data_clic)
+        else:
+            modal_edit_log(d[0], d[1], d[2], d[3], d[7], d[6])
 
 # --- 8. MAIN UI ---
 l, tk, cm, ops_list = get_cached_data("Log_Tempi"), get_cached_data("Task"), get_cached_data("Commesse"), get_cached_data("Operatori")
