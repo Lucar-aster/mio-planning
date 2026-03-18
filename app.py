24c13832263cfe1a2387769d88f5d5ef68d2378e
Lucar-aster <luca.r@astercucine.it>
2026-03-18 14:43:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -619,12 +619,12 @@ def render_gantt_fragment(df_plot, color_map, oggi_dt, x_range, delta_giorni, sh
             y_labels_pulsanti.append([c_label_pulsanti, t_label_pulsanti])
         custom_data_full.append(["LOG_FITTIZIO", r['task_id']])
         date_range = pd.date_range(start="2000-01-01", end="2100-12-31", freq='D')
-        y_val=y_labels_pulsanti[i] if st.session_state.vista_compressa else y_labels_pulsanti[i]
+        y_val=y_labels_pulsanti[i]
     
     fig.add_trace(go.Bar(
         base=date_range,
         x=[pd.Timedelta(days=1)] * len(date_range),
-        y=[y_labels_pulsanti[i]] * len(date_range),
+        y=[y_val] * len(date_range),
         orientation='h',
         width=0.9,
         offset= -0.45,
