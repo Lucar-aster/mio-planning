f83c4530d8b8b050dca7f1eaf487126292fef9e2
Lucar-aster <luca.r@astercucine.it>
2026-03-19 08:29:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ def modal_gestione_clic(task_id, data_clic):
     commessa_info = next((c for c in cm_data if c['id'] == task_info['commessa_id']), None)
     
     st.subheader()
-    with st.expander(""🏗️ Modifica Anagrafica"", expanded=False):
+    with st.expander("🏗️ Modifica Anagrafica", expanded=False):
         new_tk_name = st.text_input("Nome Task", value=task_info.get('nome_task', ''))
         new_tk_status = st.selectbox("Stato Task", options=STATI_TASK, index=STATI_TASK.index(task_info.get('stato', STATI_TASK[0])))
         if commessa_info:
