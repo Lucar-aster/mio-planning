a3f6632939174ce8f2c5554d210c45ac3b0a81d0
Lucar-aster <luca.r@astercucine.it>
2026-04-27 08:45:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -697,8 +697,6 @@ def render_gantt_fragment(df_plot, color_map, oggi_dt, x_range, delta_giorni, sh
 # --- NUOVA LOGICA: AREA DI CLIC GIORNALIERA DINAMICA ---
     # Creiamo segmenti solo per i giorni visibili (x_range) per non appesantire il browser
     click_dates = pd.date_range(start=x_range[0], end=x_range[1], freq='D')
-    click_starts = [d.replace(hour=8, minute=0) for d in click_dates]
-    durata_clic_ms = 9 * 3600 * 1000
 	
     ms_per_day = 24 * 3600 * 1000
     grid_bases, grid_xs, grid_ys, grid_customdata = [], [], [], []
@@ -723,7 +721,9 @@ def render_gantt_fragment(df_plot, color_map, oggi_dt, x_range, delta_giorni, sh
         y_labels_pulsanti.append(y_val)
         
         for d in click_dates:
-            grid_bases.append(d)
+		    click_starts = d.replace(hour=8, minute=0)
+			durata_clic_ms = 9 * 3600 * 1000
+            grid_bases.append(click_starts)
             grid_xs.append(durata_clic_ms)
             grid_ys.append(y_val)
             # Salviamo [Tipo, Task_ID, Data_Giorno]
