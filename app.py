f8e5a5fcdcc1ce51fcec448118e26c1c40e5f56e
Lucar-aster <luca.r@astercucine.it>
2026-05-12 12:03:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import hashlib
 from streamlit_calendar import calendar
 import plotly.express as px
 import io
+import locale
+
+locale.setlocale(locale.LC_ALL, "it_IT")
 
 # --- 1. CONFIGURAZIONE PAGINA E COSTANTI ---
 LOGO_URL = "https://vjeqrhseqbfsomketjoj.supabase.co/storage/v1/object/public/icona/logo.png"
