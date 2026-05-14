716a46026f3e46a51889d7081d70a3a1c18d3fab
Lucar-aster <luca.r@astercucine.it>
2026-05-14 08:13:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def local_css(file_name):
     if os.path.exists(file_name):
         with open(file_name) as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    esle:
+    else:
         st.error(f"⚠️ Attenzione: il file {file_name} non è stato trovato!")
     
 local_css("style.css")
