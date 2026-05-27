40199fcf02c485d4906a90fc1fc510474b51ed7b
Lucar-aster <luca.r@astercucine.it>
2026-05-27 08:27:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def modal_gestione_clic(task_id, data_clic):
         
     with st.expander(f"⏱️ Nuovo Log - {data_clic.strftime('%d/%m/%Y')}", expanded=True):
         nome_commessa = commessa_info.get('nome_commessa', 'Non specificata')
-        nome_task = task_info.get('nome_task, 'Senza nome')
+        nome_task = task_info.get('nome_task', 'Senza nome')
         st.info(f"📋 **Commessa:** {nome_commessa}  \n **Task:** {nome_task}")
         
         date_range_l = st.date_input("Periodo Log", value=(data_clic, data_clic), format="DD/MM/YYYY", key="date_range_l")
