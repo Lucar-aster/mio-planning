7f837f691f4bdcc2774f0a1edae9140442a53a09
Lucar-aster <luca.r@astercucine.it>
2026-08-05 09:46:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -913,12 +913,12 @@ if l and tk and cm:
     if f_s_tag: df_p = df_p[df_p['tag'].isin(f_s_tag)]
     if search_text: df_p = df_p[df_p['Commessa'].astype(str).str.lower().str.contains(search_text) | df_p['Task'].astype(str).str.lower().str.contains(search_text)]
     
-	if isinstance(f_range, (list, tuple)) and len(f_range) == 2:
-    	start_search = pd.to_datetime(f_range[0])
-    	end_search = pd.to_datetime(f_range[1])
-    	df_p['inizio'] = pd.to_datetime(df_p['inizio'])
-    	df_p['fine'] = pd.to_datetime(df_p['fine'])
-    	df_p = df_p[(df_p['inizio'] <= end_search) & (df_p['fine'] >= start_search)]
+    if isinstance(f_range, (list, tuple)) and len(f_range) == 2:
+        start_search = pd.to_datetime(f_range[0])
+        end_search = pd.to_datetime(f_range[1])
+        df_p['inizio'] = pd.to_datetime(df_p['inizio'])
+        df_p['fine'] = pd.to_datetime(df_p['fine'])
+        df_p = df_p[(df_p['inizio'] <= end_search) & (df_p['fine'] >= start_search)]
     
 tabs = st.tabs(["📊 Timeline", "📅 Calendario", "📑 Agenda", "📋 Gestione Logs", "⚙️ Gestione", "📈 Statistiche"])    
 
